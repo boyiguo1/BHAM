fa9486caef984d0379be0f5cebe1388d56983fab
Boyi Guo <boyiguo1@uab.edu>
2020-12-03 18:41:19 -0600

    Create Package Manual Page

---
+++ R/BHAM.R
@@ -0,0 +1,13 @@
+#' BHAM: A package to fit Bayesian Hierarchical Additive Models for High Dimensional Application
+#'
+#'// TODO: Update this page
+#' The foo package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name BHAM
+NULL
+#> NULL
